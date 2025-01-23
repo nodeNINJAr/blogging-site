6de7a1d29fe5da89648b5bc5cf1c1a1c08ce3f95
Mehedi hasan <nodeNINJAr@users.noreply.github.com>
2025-01-23 23:39:16 +0600

    loader added

--- src/app/(root)/profile/page.jsx
+++ src/app/(root)/profile/page.jsx
@@ -1,19 +1,22 @@
 import React from "react";
-// import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-// import { redirect } from "next/navigation";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
 
 //
-const page =  () => {
-  // const { isAuthenticated, getUser } = getKindeServerSession();
-  // const isUserAuthenticated = await isAuthenticated();
-  // const user = await getUser();
+const page = async () => {
+  const { isAuthenticated, getUser } = getKindeServerSession();
+  const isUserAuthenticated = await isAuthenticated();
+  const user = await getUser();
 
-  // //
-  // if (!isUserAuthenticated) {
-  //   return redirect("/api/auth/login");
-  // }
+  //
+  if (!isUserAuthenticated) {
+    return redirect("/api/auth/login");
+  }
 
-  return <div className="text-3xl font-semibold text-red-500 text-center mt-10 bg-slate-500 rounded-md py-4">Welcome to your profile! </div>;
+if(!user) return <p className="text-red-500 text-2xl font-medium capitalize py-20">Loading...</p>
+
+
+  return <div className="text-3xl font-semibold text-red-500 text-center mt-10 bg-slate-500 rounded-md py-4">Welcome to your profile! {user?.given_name || "User"}</div>;
 };
 
 export default page;
