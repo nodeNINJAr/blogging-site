d585fb2e5267225b557a52b9db9139f8ed5ebf56
Mehedi hasan <nodeNINJAr@users.noreply.github.com>
2025-01-24 01:49:16 +0600

    correction

--- src/app/api/auth/[kindeAuth]/route.js
+++ src/app/api/auth/[kindeAuth]/route.js
@@ -1,2 +1,6 @@
 import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
-export const GET = handleAuth();
+
+export const GET = handleAuth({
+  redirectUri: 'https://simple-blogging.vercel.app/api/auth/callback',
+  postLogoutRedirectUri: 'https://simple-blogging.vercel.app',        
+});
