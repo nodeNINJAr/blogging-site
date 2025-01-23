ec833ba8038433d5cafa97db464dc68a948581cd
Mehedi hasan <nodeNINJAr@users.noreply.github.com>
2025-01-24 02:11:21 +0600

    correction

--- src/app/api/auth/[kindeAuth]/route.js
+++ src/app/api/auth/[kindeAuth]/route.js
@@ -1,12 +1,2 @@
-import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server';
-
-export const GET = async (req, res) => {
-  try {
-    console.log('Received Callback Parameters:', req.query); // Log incoming query parameters
-      await handleAuth(req, res);
-  } catch (err) {
-    console.error('Error during callback:', err);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-};
-
+import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
+export const GET = handleAuth();
