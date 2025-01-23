6c2e06f524b274a0df63f2855f1cc79d2e213aee
Mehedi hasan <nodeNINJAr@users.noreply.github.com>
2025-01-24 03:13:02 +0600

    update

--- src/app/api/auth/[kindeAuth]/route.js
+++ src/app/api/auth/[kindeAuth]/route.js
@@ -1,25 +1,4 @@
-import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
+import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
+export const GET = handleAuth();
 
-const corsMiddleware = (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://simple-blogging.vercel.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
 
-  // Handle preflight requests
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return true;
-  }
-
-  return false;
-};
-
-const customHandleAuth = (req, res) => {
-  if (!corsMiddleware(req, res)) {
-    return handleAuth()(req, res);
-  }
-};
-
-export const GET = customHandleAuth;
-export const POST = customHandleAuth;
